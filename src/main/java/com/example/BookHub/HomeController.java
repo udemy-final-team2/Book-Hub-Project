80fc07c7ec4dff03c29027ec5502c8f50eb3fed3
Hambak-note <dudghks5722@gmail.com>
2023-02-14 11:40:39 +0900

    #6-etc(컨트롤러, util) home controller 변경, session 관련 const 클래스 추가

--- src/main/java/com/example/BookHub/HomeController.java
+++ src/main/java/com/example/BookHub/HomeController.java
@@ -1,18 +1,31 @@
 package com.example.BookHub;
 
+import com.example.BookHub.User.UserDTO;
+import com.example.BookHub.Util.SessionConst;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String Home() {
+    public String Home(Model model) {
+
+        UserDTO userDTO = (UserDTO)  httpSession.getAttribute(SessionConst.LOGIN_USER);
+
+        if (userDTO != null) {
+            model.addAttribute("userName", userDTO.getName());
+        }
+
         return "home";
     }
-
-    @GetMapping("/signin")
-    public String Login() {return "signin";}
 
     @GetMapping("/signup")
     public String Register() {return "signup";}
