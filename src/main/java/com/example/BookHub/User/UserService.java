f811175693d4e3dc07c03d911385119a94829589
new-h2 <79350383+new-h2@users.noreply.github.com>
2023-02-20 10:13:59 +0900

    일반 로그인

--- src/main/java/com/example/BookHub/User/UserService.java
+++ src/main/java/com/example/BookHub/User/UserService.java
@@ -1,10 +1,12 @@
 package com.example.BookHub.User;
 
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -21,4 +23,42 @@ public class UserService {
             userRepository.insertUserBySocial(user);
         return user;
     }
+    
+    public int insertuser(UserDTO dto) {
+		return userRepository.insertuser(dto);
+	}
+
+	public UserDTO loginuser(String email) {
+		return userRepository.loginuser(email);
+	}
+
+	public UserDTO userinfo(int id) {
+		return userRepository.userinfo(id);
+	}
+
+	public int updateuser(UserDTO dto) {
+		return userRepository.updateuser(dto);
+	}
+	
+	public int deleteuser(int id) {
+		return userRepository.deleteuser(id);
+	}
+
+	public List<UserDTO> selectUserList(int limit) {
+		return userRepository.selectUserList(limit);
+	}
+
+	public int totalUser() {
+		return userRepository.totalUser();
+	}
+
+	public List<UserDTO> selectUserList() {
+		return userRepository.selectUserList();
+	}
+
+	public List<UserDTO> selectUserList(String keyword) {
+		return userRepository.selectUserList(keyword);
+	}
+	
+    
 }
