8b3dbf23aecb8d45424a3a31c4bb91f217e21b89
alweiis <jstm1207@gmail.com>
2023-02-21 10:51:00 +0900

    #31-문서 비교 로직 추가

--- src/main/java/com/example/BookHub/Docs/DocsController.java
+++ src/main/java/com/example/BookHub/Docs/DocsController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class DocsController {
 
     private final DocsService docsService;
@@ -25,37 +26,45 @@ public class DocsController {
     // 문서 작성
     @PostMapping("/docs/write")
     public String writeDocument(DocsDTO dto, MultipartFile file) throws IOException {
-        String s3Key = docsService.upload(file);
+        String url = docsService.upload(file);
         DocsDTO saveDoc = DocsDTO.builder()
                 .folderId(dto.getFolderId())
                 .title(dto.getTitle())
                 .memo(dto.getMemo())
-                .s3Key(s3Key)
+                .s3Key(dto.getS3Key())
                 .build();
         docsService.writeDocument(saveDoc);
         return "redirect:/docs/write";
     }
 
     // 문서 조회
-    @GetMapping("/read/{documentId}")
+    @GetMapping("/docs/read/{documentId}")
     public String readDocument(@PathVariable Long documentId, Model model) {
         DocsDTO document = docsService.readDocument(documentId);
         model.addAttribute("document", document);
         return "document";
     }
 
-    // 문서 리스트
-    @GetMapping("/docs/{folder_id}")
-    public String getDocumentList(@PathVariable Long folder_id, Model model) {
-        List<DocsDTO> documentList = docsService.readDocumentList(folder_id);
+    // 폴더 문서 리스트 조회
+    @GetMapping("/docs/{folderId}")
+    public String getDocumentList(@PathVariable Long folderId, Model model) {
+        List<DocsDTO> documentList = docsService.readDocumentList(folderId);
         model.addAttribute("documentList", documentList);
         return "docs";
     }
 
     // 문서 삭제
-    @PostMapping("/delete")
+    @PostMapping("/docs/delete")
     public String deleteDocument(@RequestParam Long documentId) {
         docsService.deleteDocument(documentId);
         return "redirect:/docs/list";
+    }
+
+    // 문서 비교
+    @PostMapping("/docs/compare")
+    public String[] compareDocument(@RequestParam Long documentId1, @RequestParam Long documentId2) {
+        DocsDTO document1 = docsService.readDocument(documentId1);
+        DocsDTO document2 = docsService.readDocument(documentId2);
+        return docsService.compareDocument(document1.getS3Key(), document2.getS3Key());
     }
 }
