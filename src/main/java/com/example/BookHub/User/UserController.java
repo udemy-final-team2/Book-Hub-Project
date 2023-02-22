3d34a8eacdc0714ca48b30fff4db5dfb32553d78
alweiis <jstm1207@gmail.com>
2023-02-22 10:23:21 +0900

    #43-회원 관련 코드 수정 유저 식별자 타입 변경 및 세션 상수를 사용하도록 코드 수정

--- src/main/java/com/example/BookHub/User/UserController.java
+++ src/main/java/com/example/BookHub/User/UserController.java
@@ -12,13 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import static com.example.BookHub.Util.SessionConst.LOGIN_USER;
+
 @RequiredArgsConstructor
 @Controller
 @Slf4j
 public class UserController {
 
 	private final UserService userservice;
-	
+
 	@PostMapping("/login")
 	public String login(String email, String password, HttpSession session) {
 		// 일반로그인
@@ -31,13 +33,12 @@ public class UserController {
 		}
 
 		if (dto != null && email.equals(dto.getEmail())) {
-
 			if (password.equals(dto.getPassword())) {
-				session.setAttribute("loginid", dto.getId());
+				session.setAttribute(LOGIN_USER, dto);
 			}
 		}
-		
-		if(dto.getRole().equals("관리자")) {
+
+		if(dto != null && dto.getRole().equals("관리자")) {
 			view = "usermanage";
 			log.info("관리자페이지이동");
 		}else {
@@ -45,40 +46,36 @@ public class UserController {
 		}
 		return view;
 	}
-	
-	
+
+
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		// 희진_ 로그아웃 테스트용 jsp에도 버튼 추가
-		// 추후 세션값 삭제 
-		if(session.getAttribute("loginid") != null){
-				session.removeAttribute("loginid");
-			}
-	
+		if(session.getAttribute(LOGIN_USER) != null){
+			session.invalidate();	// 세션값 삭제
+		}
 		return "home";
-
 	}
-	
+
 	@PostMapping("/signupuser")
 	public String insertuser(@ModelAttribute UserDTO dto) {
 		//일반사용자 가입
 		int result = userservice.insertuser(dto);
 		return "signin";
 	}
-	
-	
+
+
 	@GetMapping("/user/{id}")
-	public ModelAndView mypage(HttpSession session, UserDTO userdto,@PathVariable("id")int id) {
+	public ModelAndView mypage(HttpSession session, UserDTO userdto, @PathVariable("id")int id) {
 		//마이페이지 내정보 불러오기
-		//int id = (int)session.getAttribute("loginid");
 		UserDTO dto = userservice.userinfo(id);
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("dto", dto);
 		mv.setViewName("mypage");
-		
+
 		return mv;
 	}
-	
+
 	@PostMapping("/user/updateuser")
 	public String updateuser(@ModelAttribute UserDTO dto) {
 		// 일반사용자 내정보 업데이트
@@ -88,19 +85,16 @@ public class UserController {
 		}
 		return "home";
 	}
-	
+
 	@GetMapping("/user/withdraw/{id}")
-	public String deleteuser(HttpSession session,@PathVariable int id) {
-		// 일반사용자 회원탈퇴  
+	public String deleteuser(HttpSession session, @PathVariable int id) {
+		// 일반사용자 회원탈퇴
 		int result = userservice.deleteuser(id);
 		if (result == 1) {
-			session.removeAttribute("loginid");
+			session.invalidate();
 			log.info("회원삭제 완료");
 		}
-		
 		return "home";
 	}
- 
-	
-	
+
 }
