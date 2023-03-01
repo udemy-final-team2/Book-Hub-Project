65f5fd794f6262613480c4ba4ad2886c517dc076
Jiwoon <kfromh0136@Naver.com>
2023-03-01 11:07:39 +0900

    #80-etc
    기타 설정파일

--- src/main/java/com/example/BookHub/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/BookHub/Security/JwtAuthenticationFilter.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
